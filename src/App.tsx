8b4b83fa1933cbc07ff452e60e07343e4c5e800c
GrayHead <ss.zhuravlov@gmail.com>
2024-05-06 21:34:50 +0300

    hooks useRef

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,30 @@
-import React from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 import './App.css';
 
 
 const App = () => {
-  return (
-      <div>
 
-      </div>
-  );
+
+    const [x, setX] = useState<number>(0);
+    const y = useRef(0);
+
+    console.log('work');
+
+    return (
+        <>
+            <button onClick={() => {
+
+                setX((x) => x + 1);
+            }}>change x {x}
+            </button>
+
+            <button onClick={() => {
+                y.current++;
+                console.log(y);
+            }}>change y - {y.current}
+            </button>
+        </>
+    );
 };
 
 export default App;
