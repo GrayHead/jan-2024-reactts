9227a0881104d2682fa8d8b394c949d2d4272c36
GrayHead <ss.zhuravlov@gmail.com>
2024-05-06 21:11:53 +0300

    hooks useEffect

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,26 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 
 
 const App = () => {
-  return (
-      <div>
 
-      </div>
-  );
+    const [counter, setCounter] = useState<number>(0);
+    let [x, setX] = useState<number>(0);
+    useEffect(() => {
+        setTimeout(() => {
+            setCounter(counter + 1);
+        }, 2000);
+    }, [x]);
+    console.log('.');
+
+    return (
+        <div>
+            <h2>{counter}</h2>
+            <button onClick={() => {
+                setX((x) => x + 1);
+            }}></button>
+        </div>
+    );
 };
 
 export default App;
