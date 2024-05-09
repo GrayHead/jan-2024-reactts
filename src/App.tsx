b7831609360c66dcd6aff30bf1a9e9c9a4db493b
GrayHead <ss.zhuravlov@gmail.com>
2024-05-09 19:50:33 +0300

    state lifting

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,45 @@
-import React from 'react';
+import React, {FC} from 'react';
 import './App.css';
 
 
-const App = () => {
-  return (
-      <div>
+const ChildComponent3: FC<any> = ({foobar}) => {
+    return <div>
+        <button onClick={() => {
+            foobar('okten');
+        }}>click me
+        </button>
 
-      </div>
-  );
+    </div>
+}
+
+const ChildComponent2: FC<any> = ({foobar}) => {
+    return <div>
+        <ChildComponent3 foobar={foobar}/>
+
+    </div>
+}
+
+const ChildComponent1: FC<any> = ({foobar}) => {
+
+    foobar('ashdasjhdj');
+    return <div>
+
+        <ChildComponent2 foobar={foobar}/>
+    </div>
+}
+
+const App = () => {
+
+    const foobar = (arg1: any) => {
+        console.log(arg1);
+    }
+
+
+    return (
+        <div>
+            <ChildComponent1 foobar={foobar}/>
+        </div>
+    );
 };
 
 export default App;
