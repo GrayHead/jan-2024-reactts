f3ed17028efba22ae113f6e72f13c3ff5f79286d
GrayHead <ss.zhuravlov@gmail.com>
2024-05-10 20:39:22 +0300

    memo

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,25 @@
-import React from 'react';
+import React, {memo, useState} from 'react';
 import './App.css';
 
+const A = memo(() => {
+    console.log('A render');
+    return <div>hello world</div>
+});
 
 const App = () => {
-  return (
-      <div>
+    console.log('App render');
+    const [counter, setCounter] = useState(0);
 
-      </div>
-  );
+    return (
+        <div>
+            <A/>
+
+            <button onClick={() => {
+                setCounter(counter + 1);
+            }}>click me
+            </button>
+        </div>
+    );
 };
 
 export default App;
