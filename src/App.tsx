31f8257e6885ee6454e25417e10e1d5a28ddc6ea
GrayHead <ss.zhuravlov@gmail.com>
2024-05-10 20:47:06 +0300

    useCallback

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,23 @@
-import React, {memo, useState} from 'react';
+import React, {FC, memo, useCallback, useState} from 'react';
 import './App.css';
 
-const A = memo(() => {
+const A: FC<any> = memo(({foobar}) => {
     console.log('A render');
+    foobar();
     return <div>hello world</div>
 });
 
 const App = () => {
     console.log('App render');
     const [counter, setCounter] = useState(0);
+    const foobar = useCallback(() => {
+        //....
+    }, [counter]);
+
 
     return (
         <div>
-            <A/>
+            <A foobar={foobar}/>
 
             <button onClick={() => {
                 setCounter(counter + 1);
