77e7239c0261342cda4a158db7cc6960ed1e1841
GrayHead <ss.zhuravlov@gmail.com>
2024-05-13 21:10:03 +0300

    useForm basic validation

--- src/components/FormComponent.tsx
+++ src/components/FormComponent.tsx
@@ -8,8 +8,11 @@ interface IFormProps {
 }
 
 const FormComponent: FC = () => {
-    let formObj = useForm<IFormProps>();
-    let {register, handleSubmit} = formObj;
+    let {
+        register,
+        handleSubmit,
+        formState: {errors}
+    } = useForm<IFormProps>();
 
 
     const save = (formValues: IFormProps) => {
@@ -19,9 +22,30 @@ const FormComponent: FC = () => {
     return (
         <div>
             <form onSubmit={handleSubmit(save)}>
-                <input type="text"  {...register('username')}/>
+                <input
+                    type="text"
+                    {...register('username',
+                        {
+                            required: {
+                                value: true,
+                                message: 'this field is required'
+                            },
+                            maxLength: {
+                                value: 10,
+                                message: 'max lenght is 10'
+                            }
+
+                        }
+                    )}
+                />
+
+                {errors.username && <span>{errors.username.message}</span>}
+                <br/>
+
                 <input type="number"  {...register('age')}/>
+                <br/>
                 <input type="text"  {...register('password')}/>
+                <br/>
                 <button>save</button>
             </form>
         </div>
