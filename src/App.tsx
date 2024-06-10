22ecd13e8885aca6283142bfb2a815c3387dbf72
GrayHead <ss.zhuravlov@gmail.com>
2024-06-10 21:19:39 +0300

    redux base

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,35 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
+
+import {useAppDispatch, useAppSelector} from "./redux/store";
+import {decrement, increment} from "./redux/slices/slice1";
 
 
 const App = () => {
-  return (
-      <div>
 
-      </div>
-  );
+    const {xxx} = useAppSelector((state) => state.slice1);
+    const dispatch = useAppDispatch();
+
+
+    return (
+        <div>
+
+            <h2>{xxx}</h2>
+            <button onClick={() => {
+                dispatch(increment(100));
+            }}>increment
+            </button>
+            <button
+                onClick={() => {
+
+                    dispatch(decrement());
+
+                }}
+            >decrement
+            </button>
+
+        </div>
+    );
 };
 
 export default App;
